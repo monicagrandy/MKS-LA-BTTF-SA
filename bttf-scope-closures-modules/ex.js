d5ca6075383db59da77e8d111198406574662b1a
Monica Grandy <monica.grandy@gmail.com>
2016-04-26 17:10:44 -0700

    updates modules and prototypes

--- bttf-scope-closures-modules/ex.js
+++ bttf-scope-closures-modules/ex.js
@@ -4,7 +4,43 @@
 // - at least 2 public methods on the API
 
 
-var theModule = /* .. */;
+
+var theModule = (function theModule(){
+		var name;
+		var age;
+		
+		var publicAPI = {
+			start: start,
+			makeName: makeName,
+			name: name,
+			age: age
+		};
+		
+		return publicAPI;
+		
+		function start(fullName, currentAge){
+			name = fullName;
+			age = currentAge;
+			incrementAge()
+			sayHello()
+		}
+		
+		function makeName(){
+			return name
+		}
+		
+		function incrementAge(){
+			age += 1
+		}
+		
+		function sayHello(){
+			var greet = "Hello, my name is " + name
+			console.log(greet)
+		}
+})();
+
+theModule.start("Monica", 27)
+theModule.makeName()
 
 
 
--- bttf-this-prototypes/ex.js
+++ bttf-this-prototypes/ex.js
@@ -11,3 +11,19 @@ var B = new A();
 B.name = "Billy";
 
 B.speak();		// Hello, my name is Billy"
+
+
+var A = {
+	
+	setName: function(name){
+		this.name = name;
+	},
+	
+	speak: function(){
+		console.log("Hello, my name is " + this.name)
+	}
+}
+
+var B = Object.create(A)
+B.setName("Billy")
+B.speak();
